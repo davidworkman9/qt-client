5dc00ff7d38df717182b94e234737f675816bbfc
Gil Moskowitz <gilmoskowitz@acm.org>
2019-06-04 11:10:48 -0400

    [35189] prevent itemList crashes when called out of usual context

--- widgets/itemCluster.cpp
+++ widgets/itemCluster.cpp
@@ -1145,7 +1145,7 @@ void itemList::sFillList()
 {
   QStringList clauses;
   clauses = _extraClauses;
-  if (!_parent->extraClause().isEmpty())
+  if (_parent && !_parent->extraClause().isEmpty())
     clauses << _parent->extraClause();
   if(!(_itemType & ItemLineEdit::cActive) && !_showInactive->isChecked())
     clauses << "(item_active)"; 
